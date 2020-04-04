d487d825485bb47de698c17fc70bac5bacfc595e
Saul Everardo Huerta Perez <Saul@Sauls-MBP.attlocal.net>
2020-04-04 00:36:26 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,11 +72,11 @@ var fight = function(enemyName) {
 };
 
 // function to start a new game
-var startGame = function() {
+  var startGame = function() {
     // reset player stats
-  playerHealth = 100;
-  playerAttack = 10;
-  playerMoney = 10;
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
   
    for (var i = 0; i < enemyNames.length; i++) {
 
@@ -98,10 +98,39 @@ var startGame = function() {
     }
     }
     
-    // play again
-    startGame();
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
     
 };
 
+// function to end the entire game
+var endGame = function() {
+
+  window.alert("The game has now ended. Let's see how you did!");
+
+  //if player is still alive, player wins!
+  if (playerHealth > 0) {
+
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+
+  } 
+  else {
+
+    window.alert("You've lost your robot in battle.");
+
+  }
+// ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    // restart the game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+  
+};
+
 // start the game when the page loads
-debugger startGame();
+startGame();
