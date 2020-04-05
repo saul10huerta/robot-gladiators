562ad0bd575e2cf044ec68cf1bff2481a8deaca7
Saul Everardo Huerta Perez <Saul@Sauls-MBP.attlocal.net>
2020-04-05 17:47:16 -0500

    save and load highsore feature finished

--- assets/js/game.js
+++ assets/js/game.js
@@ -150,6 +150,22 @@ var endGame = function() {
 
   window.alert("The game has now ended. Let's see how you did!");
 
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  
+  highScore = highScore || 0;
+
+  // if player have more money than the high score, player has new high score!
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } 
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
   //if player is still alive, player wins!
   if (playerInfo.health > 0) {
 
